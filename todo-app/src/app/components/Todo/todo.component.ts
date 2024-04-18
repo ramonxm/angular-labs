d35ac11c4b40957de8d22433a27c7b7c1d97633b
Ramon Xavier <ramon1234rxm@gmail.com>
2024-04-17 23:49:53 -0300

    feat: adding methods to add, completed and remove todo

--- todo-app/src/app/components/Todo/todo.component.ts
+++ todo-app/src/app/components/Todo/todo.component.ts
@@ -1,16 +1,18 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { Todo } from "../../models/Todo";
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: "todos",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: "./todo.component.html",
   styleUrl: "./todo.component.scss",
 })
 export class TodosComponent {
   todos!: Todo[];
+  inputValue!: string;
 
   ngOnInit(): void {
     this.todos = [
@@ -25,5 +27,49 @@ export class TodosComponent {
         completed: true,
       },
     ];
+  }
+
+  private findById(id: string) {
+    const todoIndex = this.todos.findIndex((todo) => todo.id === id);
+
+    return todoIndex;
+  }
+
+  onAdd() {
+    if (!this.inputValue) {
+      alert("Precisa adicionar um texto");
+
+      return;
+    }
+
+    const newTodo = {
+      id: crypto.randomUUID(),
+      text: this.inputValue,
+      completed: false,
+    } satisfies Todo;
+
+    this.todos.push(newTodo);
+
+    this.inputValue = "";
+  }
+
+  onCompleted(id: string) {
+    const todoIndex = this.findById(id);
+
+    if (todoIndex === -1) {
+      alert("Todo não encontrado!");
+    }
+
+    this.todos[todoIndex] = { ...this.todos[todoIndex], completed: true };
+  }
+
+  onRemove(id: string) {
+    const todoIndex = this.findById(id);
+
+    if (todoIndex === -1) {
+      alert("Todo não encontrado!");
+    }
+
+    this.todos.splice(todoIndex, 1);
   }
 }
